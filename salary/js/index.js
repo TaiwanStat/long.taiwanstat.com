37891da1b033304b6c125a7dbaa228d6f76fca67
Leo <a53378@yahoo.com.tw>
2015-07-03 02:02:37 +0800

    add function circlesort

--- salary/js/index.js
+++ salary/js/index.js
@@ -1,8 +1,8 @@
-var pack = d3.layout.pack();
+
 var width=$(".six.wide.column").width();
 var leftwidth=$(".four.wide.column").width();
-if(width>800) width=800; 
-if(width<500) width=500; 
+if(width>800) width=800;
+if(width<500) width=500;
 if(leftwidth>800) leftwidth=800;
 if(leftwidth<500) leftwidth=500;
 var leftheight=leftwidth*1/3;
@@ -11,25 +11,41 @@ var svgNum=d3.select(".numbers").append("svg").attr("width",leftwidth).attr("hei
 var svgSalary=d3.select(".salary").append("svg").attr("width",leftwidth).attr("height",leftheight);
 var svgTime=d3.select(".time").append("svg").attr("width",leftwidth).attr("height",leftheight);
 var colorScale=d3.scale.category20();
-var padding=(leftheight-80)/2; 	 
+var padding=(leftheight-80)/2;
 var widthpadding=60;
 var yScale=d3.scale.linear().domain([0,2]).range([padding,leftheight-padding]);
 var xScale=d3.scale.linear().domain([0,0]).range([widthpadding,leftwidth-widthpadding]);
 
-
 d3.csv("salary.csv",function(data){
-$(".ui.button").click(visualize);
+$('.ui.checkbox').checkbox('behavior', 'set enabled');
+$(".ui.button").click(function(){
+	$(".ui.button").each(function(){
+		$(this).data("clicked",false);
+	});
+	$(this).data("clicked",true);
+	visualize();
+});
+
+
+var force;
+var circles;
 var salaryMM,numberMM,timeMM;
-var tmp=data.map(function(d){return parseInt(d.人數總計);})
-numberMM=[d3.min(tmp),d3.max(tmp)];
-tmp=data.map(function(d){return parseInt(d.薪資平均);})
-salaryMM=[d3.min(tmp),d3.max(tmp)];
-tmp=data.map(function(d){return parseFloat(d.工時平均);})
-timeMM=[d3.min(tmp),d3.max(tmp)];
+var itemName;
+var circleScale=d3.scale.sqrt().range([8,40]).domain([0,0]);
+var tmp1=data.map(function(d){return parseInt(d.人數總計);})
+numberMM=[d3.min(tmp1),d3.max(tmp1)];
+var tmp2=data.map(function(d){return parseInt(d.薪資平均);})
+salaryMM=[d3.min(tmp2),d3.max(tmp2)];
+var tmp3=data.map(function(d){return parseFloat(d.工時平均);})
+timeMM=[d3.min(tmp3),d3.max(tmp3)];
+
 function visualize(){
 	d3.selectAll("circle").remove();
-	var itemName=$(this).text();
-	var circleScale;
+	$(".ui.button").each(function(){
+		if($(this).data("clicked")){
+			itemName=$(this).text();
+		}
+	});
 	var max=0,min=1000000;
 	var nodes=data.map(function(d){
 					return{
@@ -43,7 +59,7 @@ function visualize(){
 						else
 							return parseFloat(d[itemName+"平均"]);
 					}
-				
+
 			});
 	if(itemName=="人數")
 		circleScale=d3.scale.sqrt().range([8,40]).domain(numberMM);
@@ -51,19 +67,17 @@ function visualize(){
 		circleScale=d3.scale.sqrt().range([8,40]).domain(salaryMM);
 	else
 		circleScale=d3.scale.sqrt().range([8,40]).domain(timeMM);
-	var circles=d3.select(".visual svg").selectAll("circle").data(nodes).enter().append("circle").on("mouseover",mouseenter);//.on("mouseout",out);
-  	var force = d3.layout.force() // 建立 Layout
+	circles=d3.select(".visual svg").selectAll("circle").data(nodes).enter().append("circle").on("mouseover",mouseenter);//.on("mouseout",out);
+  force = d3.layout.force() // 建立 Layout
     		.nodes(nodes)               // 綁定資料
     		.size([width,width])            // 設定範圍
-		.charge(-60)
+				.charge(-60)
     		.on("tick", tick)           // 設定 tick 函式
     		.start();                   // 啟動！
 	function mouseenter(d){
 		d3.selectAll("circle").classed("selected",false);
 		force.charge(-60);
 		force.start();
-		console.log(this);
-		console.log(d);
 		d3.select(this).classed("selected",true);
 		force.charge(
 			function(d2){
@@ -86,12 +100,12 @@ function visualize(){
       		cx: function(it) { return it.x; },  // it.x 由 Force Layout 提供
       		cy: function(it) { return it.y; },  // it.y 由 Force Layout 提供
       		r: function(it){return circleScale(it.value); },
-		fill:function(d){return colorScale(d.type);},	 
-    		stroke: "#444",                    
+					fill:function(d){return colorScale(d.type);},
+    			stroke: "#444",
     		})
   	}
-	$("circle").mouseover();
 }
+$(".ui.button.people").click();
 function showDetail(d){
 	var theData;
 	for(var i in data){
@@ -128,7 +142,7 @@ function createRect(itemName,rectData){
 			y:function(d,i){return yScale(i);},
 			height:20,
 			width:function(d){return xScale(d[1])},
-			fill:function(d){var color=0.8-(xScale(d[1])/leftwidth)*0.7; 
+			fill:function(d){var color=0.8-(xScale(d[1])/leftwidth)*0.7;
 				return d3.hsl(200,color,color);}
 		});
 	d3.select(itemName).selectAll("text").data(rectData).enter().append("text")
@@ -142,7 +156,7 @@ function createRect(itemName,rectData){
 			x:0,
 			y:0,
 		})
-	
+
 	d3.select(itemName).selectAll("text.value").data(rectData).enter().append("text")
 		.attr({
 			"class":"value",
@@ -151,7 +165,7 @@ function createRect(itemName,rectData){
 		})
 		.text(function(d){return d[1]+rank(d);})
 
-	
+
 }
 function changeRect(itemName,rectData){
 	d3.select(itemName).selectAll("rect").data(rectData).transition().duration(250)
@@ -160,7 +174,7 @@ function changeRect(itemName,rectData){
 			y:function(d,i){return yScale(i);},
 			height:20,
 			width:function(d){return xScale(d[1])},
-			fill:function(d){var color=0.8-(xScale(d[1])/leftwidth)*0.7; 
+			fill:function(d){var color=0.8-(xScale(d[1])/leftwidth)*0.7;
 				return d3.hsl(200,color,color);}
 		});
 	d3.select(itemName).selectAll("text.value").data(rectData).transition().duration(250)
@@ -169,6 +183,7 @@ function changeRect(itemName,rectData){
 			y:function(d,i){return yScale(i)+15;}, //the rect center is diff from text
 		})
 		.text(function(d){return d[1]+rank(d);})
+
 }
 function rank(item){
 	item[0]=item[0].replace("(","");
@@ -179,5 +194,55 @@ function rank(item){
 	})
 	rankArr.sort(function(a,b){return b-a;})
 	return " (No."+(rankArr.indexOf(parseFloat(item[1]))+1)+")";
-}	
+}
+function circlesort(){
+	var dataTmp;
+	if(itemName=="人數"){
+			dataTmp=tmp1;
+	}
+	else if (itemName=="薪資") {
+		dataTmp=tmp2;
+	}
+	else {
+		dataTmp=tmp3;
+	}
+	dataTmp.sort(function(a,b){return b-a;});
+	$(".visual svg").attr("height",width*3);
+	force.size([width,1200])
+				.charge(0)
+    		.on("tick", tick)           // 設定 tick 函式
+    		.start();                   // 啟動！
+	function tick() { // tick 會不斷的被呼叫
+    		circles.transition().duration(50).attr({
+      		cx: function(it) { return (dataTmp.indexOf(it.value)%(Math.floor(width/80)))*80+40; },
+      		cy: function(it) { return Math.floor(dataTmp.indexOf(it.value)/(width/80))*80+40; },
+      		r: function(it){return circleScale(it.value); },
+					fill:function(d){return colorScale(d.type);},
+    			stroke: "#444",
+    		})
+  	}
+}
+$('.ui.checkbox')
+  .checkbox()
+  .first().checkbox({
+    onChecked: function() {
+			circlesort();
+    },
+    onUnchecked: function() {
+			$(".visual svg").attr("height",width);
+				force.size([width,width])
+							.charge(-60)
+			    		.on("tick", tick)           // 設定 tick 函式
+			    		.start();                   // 啟動！
+				function tick() { // tick 會不斷的被呼叫
+			    		circles.transition().duration(50).attr({
+			      		cx: function(it) { return it.x; },
+			      		cy: function(it) { return it.y; },
+			      		r: function(it){return circleScale(it.value); },
+								fill:function(d){return colorScale(d.type);},
+			    			stroke: "#444",
+			    		})
+			  	}
+    }
+  });
 });
