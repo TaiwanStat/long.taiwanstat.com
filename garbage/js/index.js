4cd3e4495e092667eee193126acc8f297f43932f
Leo <a53378@yahoo.com.tw>
2015-07-23 16:21:48 +0800

    edit text display

--- garbage/js/index.js
+++ garbage/js/index.js
@@ -34,23 +34,23 @@ d3.csv("year_data.csv",function(dataset){
 function visual(dataset_arr){
     var arc_g = pie_g.selectAll("g").data(pie(dataset_arr[0])).enter().append("g").attr("class","arc_g");
     var arc_path = arc_g.append("path").attr("class","arc_path").attr("d",arc)
-        .attr("fill",function(d){return color_scale(d.data.key);}).each(function(d){this._current = d;});
+    .attr("fill",function(d){return color_scale(d.data.key);}).each(function(d){this._current = d;});
     /*var arc_path = pie_g.selectAll("path").data(pie(dataset_arr[0])).enter().append("path")
-        .attr("class","arc_path").attr("d",arc)
-        .attr("fill",function(d){return color_scale(d.data.key);})
-        .each(function(d){this._current = d;});*/
+    .attr("class","arc_path").attr("d",arc)
+    .attr("fill",function(d){return color_scale(d.data.key);})
+    .each(function(d){this._current = d;});*/
     var arc_text = arc_g.append("text")
-          .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-          .attr("dy", ".35em")
-          .style("text-anchor", "middle")
-          .text(function(d) {
-              if(d.data.value>10000){
-                  return d.data.key;
-              }
-              else{
-                  return "";
-              }
-          });
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function(d) {
+        if(d.data.value>400000){
+            return d.data.key;
+        }
+        else{
+            return "";
+        }
+    });
 
     var index = 0;
     info(dataset_arr,index);
@@ -79,27 +79,27 @@ function info(dataset_arr,index){
     });
 }
 function change(dataset_arr,arc_path,arc_text,index,interval){
-   if(index == 8){
+    if(index == 8){
         clearTimeout(interval);
     }
     arc_path = arc_path.data(pie(dataset_arr[index]));
     arc_path.transition().duration(700).attrTween("d",arcTween);
-    arc_text = arc_text.data(pie(dataset_arr[index])).text(function(d) {
-            if(d.data.value>10000){
-                return d.data.key;
-            }
-            else{
-                return "";
-            }
-        });
+    arc_text = arc_text.data(pie(dataset_arr[index])).transition().duration(10).text(function(d) {
+        if(d.data.value>400000){
+            return d.data.key;
+        }
+        else{
+            return "";
+        }
+    });
     arc_text.transition().duration(700) .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
     //arc_g.transition().duration(750).attrTween("d",arcTween); //redraw
 }
 function arcTween(a){
-        //console.log(this._current);
-        var i = d3.interpolate(this._current, a);
-        this._current = i(0);
-        return function(t) {
-            return arc(i(t));
-        };
+    //console.log(this._current);
+    var i = d3.interpolate(this._current, a);
+    this._current = i(0);
+    return function(t) {
+        return arc(i(t));
+    };
 }
