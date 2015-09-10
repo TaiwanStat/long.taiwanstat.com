b59fabd05a805b883bd9aed4ae9669f88a439a6d
chihsuan <chihsuan.huang.7@gmail.com>
2015-09-11 02:11:58 +0800

    update footer

--- include/partials/footer.js
+++ include/partials/footer.js
@@ -1,7 +1,19 @@
+
 module.exports= function (hbs) {
 
-  var b_footer = '<script src="/js/main.js"></script>'
-  // register a partial
-  hbs.registerPartial('footer', b_footer);
+	var footer = '<footer class="mdl-mini-footer">';
+	    footer += '<div class="mdl-mini-footer__left-section">© 2015 <a href="#">用數據看台灣團隊</a> | ';
+	    footer += '<a href="http://nckunetdb.appspot.com">NCKU-NetDB</a>';
+	    footer += '{{#if chart_description.collaborators}}{{#each chart_description.collaborators}} | ';
+	    footer += '<a href="{{url}}">{{name}}</a>{{/each}}{{/if}}';
+	    footer += '</div>';
+ 			footer += '<div class="mdl-mini-footer__right-section">';
+ 			footer += '<span class="footer_msg">商業合作提案、客製化圖表製作、意見回饋</span></a>歡迎來信: <a href="mailto:contact@taiwanstat.com">contact@taiwanstat.com</a></div>';
+ 			footer += '</footer>';
+ 			footer += '<script src="/js/main.js"></script>';
 
-}
+	
+  // register a partial
+  hbs.registerPartial('footer', footer);
+
+};
