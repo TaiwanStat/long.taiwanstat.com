eba4eab74da61c83b4f9c22d8d4b1b1e267b7141
chihsuan <chihsuan.huang.7@gmail.com>
2015-09-28 14:36:15 +0800

    update

--- dengue-animation/scripts/tmp.py
+++ dengue-animation/scripts/tmp.py
@@ -5,7 +5,8 @@ new = [['日期', '里別', 'Latitude', 'Longitude']]
 data = data[1:]
 from datetime import datetime
 for item in data:
-    item[7] = datetime.strptime('2015/'+item[7], '%Y/%m/%d').date()
-    item[7] = item[7].strftime('%Y/%m/%d')
-    new.append([item[7], item[4], item[-2], item[-1]])
+    if item[-5] == '陽性':
+        item[7] = datetime.strptime('2015/'+item[7], '%Y/%m/%d').date()
+        item[7] = item[7].strftime('%Y/%m/%d')
+        new.append([item[7], item[4], item[-2], item[-1]])
 csv_io.write_csv('dengue.csv', new)
