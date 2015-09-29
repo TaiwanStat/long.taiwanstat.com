48dcf994dc6a4e2b7927ff75b484d908dda5c882
chihsuan <chihsuan.huang.7@gmail.com>
2015-09-29 19:51:08 +0800

    update

--- dengue-animation/js/index.js
+++ dengue-animation/js/index.js
@@ -14,7 +14,7 @@
       info = L.control(),
       showDrug = false,
       from = new Date('2015/06/01'),
-      end = new Date('2015/09/27'),
+      end = new Date('2015/09/29'),
       pivot,
       diffDays,
       latlngs = {},
@@ -44,7 +44,7 @@
       var key = pivot.toISOString().substring(0, 10).replace(/-/g, '/');
       $('.current').text(key); 
       var latestDate = new Date()
-      latestDate.setDate(pivot.getDate()+1);
+      latestDate.setDate(pivot.getDate());
       $('.updateAt').text(latestDate.toLocaleDateString());
       threeCircleData = format(data[key].three);
       fiveCircleData = format(data[key].five);
