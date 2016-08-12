27053c5cc3504bd4a6e40283c9d06c2504913043
chihsuan <chihsuan.tw@gmail.com>
2016-08-12 08:49:11 +0000

    update

--- chart_item.js
+++ chart_item.js
@@ -9,6 +9,8 @@ post_arr.push(lists);
 
 lists.data.page.forEach(function(p) {
   if(!url.parse(p.url).protocol) {
+  	//p.img = 'https://s3-ap-northeast-1.amazonaws.com/static.twstat.com/statistics/' + p.img;
+
     post_arr.push({
       "data": {
         "chart_description": p,
